9220e0a0feb48692cba82624b2faa8ea0228311e
rriazn <rnriazanov@gmail.com>
2026-06-10 09:57:11 +0200

    Add pipelined CPU

--- src/fibo.asm
+++ src/fibo.asm
@@ -1,13 +1,26 @@
 li t0, 0
 li t1, 1
-li t4, 0x2000
+lui t4, 2
+nop
+nop
+nop
 loop_start: sw t0, 0(t4)
 add t2, t0, t1
 addi t0, t1, 0
+nop
+nop
 addi t1, t2, 0
 li t3, 2
+nop
+nop
+nop
 wait_start: addi t3, t3, -1 
+nop
+nop
+nop
 bnez t3, wait_start
 nop
+nop
 j loop_start
+nop
 nop
